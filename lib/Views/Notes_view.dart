3718a5708eed910fdd2246be7408513642723c9e
Mohamed Maher <141060515+MohamedMaherTawfik@users.noreply.github.com>
2024-09-24 13:53:11 +0300

    add fonts

--- lib/Views/Notes_view.dart
+++ lib/Views/Notes_view.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/Views/widgets/Notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notes'),
-      ),
-    );
+    return NotesViewBody();
   }
 }
